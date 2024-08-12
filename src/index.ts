50ccec2cb5b6640bbc8ea440f0c1724921548ac2
Mohammed Elhaouari <m@codiume.com>
2024-08-12 16:16:54 +0100

    Add use-singleton hook (#26)
    * Add use-singleton hook
    
    * docs(changeset): Add [use-singleton] hook

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { useQueue } from './use-queue/use-queue';
 export { useScroll } from './use-scroll/use-scroll';
+export { useSingleton } from './use-singleton/use-singleton';
---
+++ src/use-singleton/use-singleton.ts
@@ -0,0 +1,15 @@
+import { useRef } from 'react';
+
+/**
+ * React hook for creating a singleton value.
+ * @see https://github.com/Andarist/use-constant
+ */
+export function useSingleton<ValueType>(fn: () => ValueType): ValueType {
+  const ref = useRef<{ value: ValueType }>();
+
+  if (!ref.current) {
+    ref.current = { value: fn() };
+  }
+
+  return ref.current.value;
+}
