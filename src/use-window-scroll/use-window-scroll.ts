11802895b792b2e17495b2bda54bd8e7b9289f1c
NekoSakuraLucia <mokurolucia@gmail.com>
2024-12-28 18:14:56 +0700

    Add comments to useScroll, useWindowScroll

--- src/use-window-scroll/use-window-scroll.ts
+++ src/use-window-scroll/use-window-scroll.ts
@@ -5,6 +5,12 @@ type ScrollPosition = {
   y: number;
 };
 
+/**
+ * Function to scroll the window to a specific position.
+ * 
+ * @param {Partial<ScrollPosition>} position - The target position to scroll to. It can contain `x` (horizontal) and/or `y` (vertical) properties.
+ * @param {ScrollOptions} options - Optional scroll behavior settings (default is `smooth`).
+ */
 const scrollTo = (
   { x, y }: Partial<ScrollPosition>,
   options: ScrollOptions = { behavior: 'smooth' }
@@ -22,6 +28,11 @@ const scrollTo = (
   window.scrollTo(scrollOptions);
 };
 
+/**
+ * Custom hook for tracking the window's scroll position.
+ * 
+ * @returns A tuple containing the current scroll position (`x`, `y`) and a `scrollTo` function to control window scrolling.
+ */
 export function useWindowScroll() {
   const [position, setPosition] = useState<ScrollPosition>({ x: 0, y: 0 });
 
